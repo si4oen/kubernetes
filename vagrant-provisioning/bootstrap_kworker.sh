0edfbc5a7e3ef7172277766bac5e414942daa2e0
Si4oen <si4oen@users.noreply.github.com>
2020-01-17 10:56:12 +0700

    Update bootstrap_kworker.sh

--- vagrant-provisioning/bootstrap_kworker.sh
+++ vagrant-provisioning/bootstrap_kworker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Join worker nodes to the Kubernetes cluster
-echo "===== [TASK] Join node(s) to Kubernetes Cluster"
+echo ">>>>> [TASK] Join node(s) to Kubernetes Cluster"
 yum install -q -y sshpass >/dev/null 2>&1
 sshpass -p "centos" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kmaster.testlab.local:/joincluster.sh /joincluster.sh 2>/dev/null
 bash /joincluster.sh >/dev/null 2>&1
