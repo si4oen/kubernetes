6beb426acc07700a7956e1df9fc00590db41ea17
Si4oen <si4oen@users.noreply.github.com>
2020-01-17 10:42:44 +0700

    Update bootstrap_flannel.sh

--- vagrant-provisioning/bootstrap_flannel.sh
+++ vagrant-provisioning/bootstrap_flannel.sh
@@ -1,51 +1,51 @@
 #!/bin/bash
 
 ## Set TimeZone to Asia/Ho_Chi_Minh
-echo "===== [TASK] Set TimeZone to Asia/Ho_Chi_Minh"
+echo ">>>>> [TASK] Set TimeZone to Asia/Ho_Chi_Minh"
 timedatectl set-timezone Asia/Ho_Chi_Minh
 
 ## Update the system >/dev/null 2>&1
-echo "===== [TASK] Updating the system"
+echo ">>>>> [TASK] Updating the system"
 yum install -y epel-release >/dev/null 2>&1
 yum update -y >/dev/null 2>&1
 
 ## Install desired packages
-echo "===== [TASK] Installing desired packages"
+echo ">>>>> [TASK] Installing desired packages"
 yum install -y telnet htop net-tools wget nano >/dev/null 2>&1
 
 ## Enable password authentication
-echo "===== [TASK] Enabled SSH password authentication"
+echo ">>>>> [TASK] Enabled SSH password authentication"
 sed -i "s/^PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
 systemctl reload sshd
 
 ## Set Root Password
-echo "===== [TASK] Set root password"
+echo ">>>>> [TASK] Set root password"
 echo "centos" | passwd --stdin root >/dev/null 2>&1
 
 ## Disable and Stop firewalld
-echo "===== [TASK] Disable and stop firewalld"
+echo ">>>>> [TASK] Disable and stop firewalld"
 systemctl disable firewalld >/dev/null 2>&1
 systemctl stop firewalld
 
 ## Disable SELinux
-echo "===== [TASK] Disable SELinux"
+echo ">>>>> [TASK] Disable SELinux"
 setenforce 0
 sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
 
 ## Install docker from Docker-ce repository
-echo "===== [TASK] Install docker container engine"
+echo ">>>>> [TASK] Install docker container engine"
 yum install -y -q yum-utils device-mapper-persistent-data lvm2 > /dev/null 2>&1
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo > /dev/null 2>&1
 yum install -y -q docker-ce >/dev/null 2>&1
 
 ## Enable docker service
-echo "===== [TASK] Enable and start docker service"
+echo ">>>>> [TASK] Enable and start docker service"
 systemctl daemon-reload
 systemctl enable docker >/dev/null 2>&1
 systemctl start docker
 
 ## Add sysctl settings
-echo "===== [TASK] Add sysctl settings"
+echo ">>>>> [TASK] Add sysctl settings"
 cat >>/etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
@@ -53,12 +53,12 @@ EOF
 sysctl --system >/dev/null 2>&1
 
 ## Disable swap
-echo "===== [TASK] Disable and turn off SWAP"
+echo ">>>>> [TASK] Disable and turn off SWAP"
 sed -i '/swap/d' /etc/fstab
 swapoff -a
 
 ## Add yum repo file for Kubernetes
-echo "===== [TASK] Add yum repo file for kubernetes"
+echo ">>>>> [TASK] Add yum repo file for kubernetes"
 cat >>/etc/yum.repos.d/kubernetes.repo<<EOF
 [kubernetes]
 name=Kubernetes
@@ -70,17 +70,17 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cl
 EOF
 
 ## Install Kubernetes
-echo "===== [TASK] Install Kubernetes (kubeadm, kubelet and kubectl)"
+echo ">>>>> [TASK] Install Kubernetes (kubeadm, kubelet and kubectl)"
 yum install -y -q kubeadm kubelet kubectl >/dev/null 2>&1
 
 ## Start and Enable kubelet service
-echo "===== [TASK] Enable and start kubelet service"
+echo ">>>>> [TASK] Enable and start kubelet service"
 systemctl daemon-reload
 systemctl enable kubelet >/dev/null 2>&1
 systemctl start kubelet >/dev/null 2>&1
 
 ## Update hosts file
-echo "===== [TASK] Update host file /etc/hosts"
+echo ">>>>> [TASK] Update host file /etc/hosts"
 cat >>/etc/hosts<<EOF
 192.168.16.151 docker1.testlab.local docker1
 192.168.16.141 jenkins1.testlab.local jenkins1
@@ -90,7 +90,7 @@ cat >>/etc/hosts<<EOF
 EOF
 
 ## Cleanup system >/dev/null 2>&1
-echo "===== [TASK] Cleanup system"
+echo ">>>>> [TASK] Cleanup system"
 package-cleanup -y --oldkernels --count=1 >/dev/null 2>&1
 yum -y autoremove >/dev/null 2>&1
 yum clean all >/dev/null 2>&1
